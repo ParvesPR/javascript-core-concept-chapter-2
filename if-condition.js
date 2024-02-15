a33b9418ea50122c577fbc65b5de02117d784c9c
ParvesPR <parves2585@gmail.com>
2024-02-15 14:13:31 +0800

    check if condition

---
+++ if-condition.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 20;
+if (a > b) {
+    console.log('a is greater than b');
+};
+if (a < b) {
+    console.log('a smaller than b');
+}
+
+var c = 113;
+if (c % 2 === 0) {
+    console.log('c is an even number');
+};
+if (c % 2 === 1){
+    console.log('c is an odd number');
+}
