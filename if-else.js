29583f96f15c27e4b58b536160e93b9b7de825fa
ParvesPR <parves2585@gmail.com>
2024-02-15 21:44:31 +0800

    else condition

--- if-else.js
+++ if-else.js
@@ -2,15 +2,13 @@ var a = 10;
 var b = 20;
 if (a > b) {
     console.log('a is greater than b');
-};
-if (a < b) {
+} else {
     console.log('a smaller than b');
 }
 
 var c = 113;
 if (c % 2 === 0) {
     console.log('c is an even number');
-};
-if (c % 2 === 1){
+} else {
     console.log('c is an odd number');
 }
