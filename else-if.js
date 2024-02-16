87f2b98051b4c6e9f4cf328598e8d5e73f488228
ParvesPR <parves2585@gmail.com>
2024-02-16 23:44:48 +0800

    else-if condition

---
+++ else-if.js
@@ -0,0 +1,12 @@
+var a = 45;
+var b = 75;
+
+if (a > b) {
+    console.log(a + ' is greater than ' + b);
+}
+else if (a < b) {
+    console.log(a + ' is smaller than ' + b);
+}
+else if (a === b) {
+    console.log(a + ' and ' + b + ' is similar');
+}
