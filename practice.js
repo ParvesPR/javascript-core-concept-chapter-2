6cab2c1026f780689a47e84ef3980ff754122056
ParvesPR <parves2585@gmail.com>
2024-02-20 23:35:30 +0800

    practice problem using if else condition

---
+++ practice.js
@@ -0,0 +1,23 @@
+var tom = 81;
+var me = 44;
+var she = 65;
+
+var a = 80;
+var b = 70;
+var c = 60;
+var d = 40;
+if (tom >= a) {
+    console.log('tom got a grade');
+}
+else if (tom >= b) {
+    console.log('tom got b grade');
+}
+else if (tom >= c) {
+    console.log('tom got c grade');
+}
+else if (tom >= d) {
+    console.log('tom got d grade');
+}
+else {
+    console.log('tom failed');
+}
