8c78ac1e81d0c791b3216e984950a102195b5365
ParvesPR <parves2585@gmail.com>
2024-02-20 23:41:59 +0800

    simple grading system using if-else condition

--- practice.js
+++ practice.js
@@ -1,23 +1,56 @@
 var tom = 81;
-var me = 44;
-var she = 65;
+var john = 44;
+var sarlin = 65;
 
 var a = 80;
 var b = 70;
 var c = 60;
 var d = 40;
+// TOM MARK GRADING
 if (tom >= a) {
-    console.log('tom got a grade');
+    console.log('Tom got A grade');
 }
 else if (tom >= b) {
-    console.log('tom got b grade');
+    console.log('Tom got B grade');
 }
 else if (tom >= c) {
-    console.log('tom got c grade');
+    console.log('Tom got C grade');
 }
 else if (tom >= d) {
-    console.log('tom got d grade');
+    console.log('Tom got D grade');
 }
 else {
-    console.log('tom failed');
+    console.log('Tom Failed');
+}
+// JOHN MARK GRADING
+if (john >= a) {
+    console.log('John got A grade');
+}
+else if (john >= b) {
+    console.log('John got B grade');
+}
+else if (john >= c) {
+    console.log('John got C grade');
+}
+else if (john >= d) {
+    console.log('John got D grade');
+}
+else {
+    console.log('John Failed');
+}
+// SARLIN MARK GRADING
+if (sarlin >= a) {
+    console.log('John got A grade');
+}
+else if (sarlin >= b) {
+    console.log('Sarlin got B grade');
+}
+else if (sarlin >= c) {
+    console.log('Sarlin got C grade');
+}
+else if (sarlin >= d) {
+    console.log('Sarlin got D grade');
+}
+else {
+    console.log('Sarlin Failed');
 }
