d9693d21048272f71d83c3cc376d41dae1cda950
ParvesPR <parves2585@gmail.com>
2024-02-21 23:57:25 +0800

    find age using switch condition

--- practice.js
+++ practice.js
@@ -1,56 +1,78 @@
-var tom = 81;
-var john = 44;
-var sarlin = 65;
+// var tom = 81;
+// var john = 44;
+// var sarlin = 65;
 
-var a = 80;
-var b = 70;
-var c = 60;
-var d = 40;
-// TOM MARK GRADING
-if (tom >= a) {
-    console.log('Tom got A grade');
-}
-else if (tom >= b) {
-    console.log('Tom got B grade');
-}
-else if (tom >= c) {
-    console.log('Tom got C grade');
-}
-else if (tom >= d) {
-    console.log('Tom got D grade');
-}
-else {
-    console.log('Tom Failed');
-}
-// JOHN MARK GRADING
-if (john >= a) {
-    console.log('John got A grade');
-}
-else if (john >= b) {
-    console.log('John got B grade');
-}
-else if (john >= c) {
-    console.log('John got C grade');
-}
-else if (john >= d) {
-    console.log('John got D grade');
-}
-else {
-    console.log('John Failed');
-}
-// SARLIN MARK GRADING
-if (sarlin >= a) {
-    console.log('John got A grade');
-}
-else if (sarlin >= b) {
-    console.log('Sarlin got B grade');
-}
-else if (sarlin >= c) {
-    console.log('Sarlin got C grade');
-}
-else if (sarlin >= d) {
-    console.log('Sarlin got D grade');
-}
-else {
-    console.log('Sarlin Failed');
-}
+// var a = 80;
+// var b = 70;
+// var c = 60;
+// var d = 40;
+// // TOM MARK GRADING
+// if (tom >= a) {
+//     console.log('Tom got A grade');
+// }
+// else if (tom >= b) {
+//     console.log('Tom got B grade');
+// }
+// else if (tom >= c) {
+//     console.log('Tom got C grade');
+// }
+// else if (tom >= d) {
+//     console.log('Tom got D grade');
+// }
+// else {
+//     console.log('Tom Failed');
+// }
+// // JOHN MARK GRADING
+// if (john >= a) {
+//     console.log('John got A grade');
+// }
+// else if (john >= b) {
+//     console.log('John got B grade');
+// }
+// else if (john >= c) {
+//     console.log('John got C grade');
+// }
+// else if (john >= d) {
+//     console.log('John got D grade');
+// }
+// else {
+//     console.log('John Failed');
+// }
+// // SARLIN MARK GRADING
+// if (sarlin >= a) {
+//     console.log('John got A grade');
+// }
+// else if (sarlin >= b) {
+//     console.log('Sarlin got B grade');
+// }
+// else if (sarlin >= c) {
+//     console.log('Sarlin got C grade');
+// }
+// else if (sarlin >= d) {
+//     console.log('Sarlin got D grade');
+// }
+// else {
+//     console.log('Sarlin Failed');
+// }
+
+var age = 25;
+
+switch (age) {
+    case 10:
+        console.log("Age is 10");
+        break;
+    case 15:
+        console.log("Age is 15");
+        break;
+    case 20:
+        console.log("Age is 20");
+        break;
+    case 25:
+        console.log("Age is 25");
+        break;
+    case 30:
+        console.log("Age is 30");
+        break;
+    default:
+        console.log("Does not match");
+} 
