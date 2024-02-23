73e443fa62de21e868a57f23d003139d6f707564
ParvesPR <parves2585@gmail.com>
2024-02-23 15:10:50 +0800

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = '';
+b = a % 2 === 0 ? 'even' : 'odd';
+console.log(b);
+
+var a = 10;
+var b = 12;
+var result = a > b ? 'A is greater than B' : 'A is smaller than B';
+console.log(result);
